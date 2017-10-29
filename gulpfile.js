c5a74482b463182b30d321ba37e77a3a8cd38c30
Madro24 <omadrid24@gmail.com>
2017-10-29 11:48:24 -0700

    setting gulp up

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,24 @@ var gulp = require('gulp')
     browserSync = require('browser-sync').create()
     useref = require('gulp-useref')
     uglify = require('gulp-uglify')
-    gulpIf = require('gulp-if');
+    gulpIf = require('gulp-if')
+    cssnano = require('gulp-cssnano')
+    del = require('del'),
+    runSequence = require('run-sequence');
 
 var config = {
+        baseAppDir: 'app'
+        htmlPath: './app/**/*.html'
+        jsPath: 'app/js/**/*.js'
          sassPath: './app/scss/**/*.scss',
+        sassDestPath: './app/css',
          bowerDir: './bower_components',
-        sassDestPath: './app/css'
+        destPath: './dist'
     }
+
+var bowerSassPath = [
+  config.bowerDir + '/bootstrap-sass/assets/stylesheets'
+]
 
 gulp.task('bower', function() { 
     return bower()
@@ -22,9 +33,7 @@ gulp.task('sass', function() { 
     return sass([config.sassPath],
         {
              style: 'compressed',
-             loadPath: [
-                 config.bowerDir + '/bootstrap-sass/assets/stylesheets'
-             ]
+             loadPath: bowerSassPath
          }) 
             .on("error", notify.onError(function (error) {
                  return "Error: " + error.message;
@@ -35,28 +44,44 @@ gulp.task('sass', function() { 
         }))
 });
 
-
 gulp.task('watch', ['browserSync','sass'], function(){
-  gulp.watch('app/scss/**/*.scss', ['sass']);
+  gulp.watch(config.sassPath, ['sass']);
   // Reloads the browser whenever HTML or JS files change
-  gulp.watch('app/*.html', browserSync.reload);
-  gulp.watch('app/js/**/*.js', browserSync.reload);
+  gulp.watch(config.htmlPath, browserSync.reload);
+  gulp.watch(config.jsPath, browserSync.reload);
 })
 
 gulp.task('browserSync', function() {
   browserSync.init({
     server: {
-      baseDir: 'app'
+      baseDir: config.baseAppDir
     },
   })
 })
 
 gulp.task('useref', function(){
-  return gulp.src('app/*.html')
+  return gulp.src(config.htmlPath)
     .pipe(useref())
     // Minifies only if it's a JavaScript file
     .pipe(gulpIf('*.js', uglify()))
-    .pipe(gulp.dest('dist'))
+    // Minifies only if it's a CSS file
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest(config.destPath))
 });
 
-  gulp.task('default', ['bower', 'sass','watch']);
+gulp.task('clean:dist', function() {
+  return del.sync(config.destPath);
+})
+
+gulp.task('build', function (callback) {
+  runSequence('clean:dist',
+    ['sass', 'useref'],
+    callback
+  )
+})
+
+gulp.task('default', function (callback) {
+  runSequence(['bower','sass','browserSync', 'watch'],
+    callback
+  )
+})
